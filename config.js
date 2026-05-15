b276e1270ac4d0dc4bbf768c050b2b687779bd8f
Kaph2194 <163620505+Kaph2194@users.noreply.github.com>
2026-05-15 13:56:44 -0500

    V22026

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 
 const SHEETS_CONFIG = {
   // URL del Google Apps Script desplegado como Web App
-  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbw4YQIjFRKcAV5rsLRCHotoBzJZ-eugs_YEN_Wq1lYU94VSnO0jRR2PUDVLdQTnZc1x/exec",
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxNi4zzjv-OkuqxGqMPb-jkJK-4qqd6oVYu7nRPk9X0Yc7oqzZiU_gmLD1v7jUuAQ_CVQ/exec",
 
   // PIN del administrador (placa: ADMIN)
   ADMIN_PIN: "admin123"
