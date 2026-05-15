597fd49cf0f2580a8f77c6e7ce786fc0d6f10a85
Kaph2194 <163620505+Kaph2194@users.noreply.github.com>
2026-05-15 12:31:50 -0500

    Update SCRIPT_URL in config.jsv4

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 
 const SHEETS_CONFIG = {
   // URL del Google Apps Script desplegado como Web App
-  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzZwEmIhPZYSCB_AU4sD5rMGU2t6L1hLvk_w7JCrUzUxUTgtRZgf6BhWOGwT-3CyZ4/exec",
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwERh4qh1rC2pq5By8N7jSRBMT7UQj-5Ny_1A3zo2k9SC6GdueuQ0KdYwVorbT6DsI/exec",
 
   // PIN del administrador (placa: ADMIN)
   ADMIN_PIN: "admin123"
